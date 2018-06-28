2b34ba8af88e2a28027d1c0924144272675268e0
Querz <admin@querz.net>
2018-06-28 11:16:09 +0200

    limit goto dialog input

--- src/main/java/net/querz/mcaselector/GotoDialog.java
+++ src/main/java/net/querz/mcaselector/GotoDialog.java
@@ -4,28 +4,34 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 import net.querz.mcaselector.util.Point2i;
 
 public class GotoDialog extends Dialog<Point2i> {
 
 	private Point2i value;
+	private Integer x, z;
 	private TextField xValue, zValue;
 
-	public GotoDialog() {
+	public GotoDialog(Stage primaryStage) {
 		setTitle("Goto location");
+		getDialogPane().getStyleClass().add("goto-dialog-pane");
+		getDialogPane().getStylesheets().addAll(primaryStage.getScene().getStylesheets());
+
 		setResultConverter(p -> value);
 
 		getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
 		getDialogPane().lookupButton(ButtonType.OK).setDisable(true);
 
 		HBox box = new HBox();
+		box.getStyleClass().add("coordinate-box");
 
 		xValue = new TextField();
 		xValue.setPromptText("X");
-		xValue.textProperty().addListener((a, b, c) -> onInput(c, zValue.getText()));
+		xValue.textProperty().addListener((a, o, n) -> onXInput(o, n));
 		zValue = new TextField();
 		zValue.setPromptText("Z");
-		zValue.textProperty().addListener((a, b, c) -> onInput(xValue.getText(), c));
+		zValue.textProperty().addListener((a, o, n) -> onZInput(o, n));
 
 		box.getChildren().addAll(xValue, zValue);
 		getDialogPane().setContent(box);
@@ -33,13 +39,40 @@ public class GotoDialog extends Dialog<Point2i> {
 		xValue.requestFocus();
 	}
 
-	private void onInput(String xValue, String zValue) {
+	//allow empty textfield
+	//allow only + / -
+
+	private void onXInput(String o, String n) {
 		try {
-			value = new Point2i(Integer.parseInt(xValue), Integer.parseInt(zValue));
-			getDialogPane().lookupButton(ButtonType.OK).setDisable(false);
+			x = Integer.parseInt(n);
 		} catch (NumberFormatException ex) {
-			getDialogPane().lookupButton(ButtonType.OK).setDisable(true);
+			x = null;
+			if (!"-".equals(n) && !"+".equals(n) && !"".equals(n)) {
+				xValue.setText(o);
+			}
+		}
+		setValue();
+	}
+
+	private void onZInput(String o, String n) {
+		try {
+			z = Integer.parseInt(n);
+		} catch (NumberFormatException ex) {
+			z = null;
+			if (!"-".equals(n) && !"+".equals(n) && !"".equals(n)) {
+				zValue.setText(o);
+			}
+		}
+		setValue();
+	}
+
+	private void setValue() {
+		if (x == null || z == null) {
 			value = null;
+			getDialogPane().lookupButton(ButtonType.OK).setDisable(true);
+		} else {
+			value = new Point2i(x, z);
+			getDialogPane().lookupButton(ButtonType.OK).setDisable(false);
 		}
 	}
 }
