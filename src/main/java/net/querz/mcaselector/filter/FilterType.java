20ca71559dbcd4da5b2d1b38a5442fcb851b5693
Querz <admin@querz.net>
2019-11-29 10:57:54 +0100

    remove space for entity amount filter

--- src/main/java/net/querz/mcaselector/filter/FilterType.java
+++ src/main/java/net/querz/mcaselector/filter/FilterType.java
@@ -13,7 +13,7 @@ public enum FilterType {
 	STATUS("Status", StatusFilter.class, Format.TEXT),
 	LIGHT_POPULATED("LightPopulated", LightPopulatedFilter.class, Format.NUMBER),
 	ENTITIES("Entities", EntityFilter.class, Format.TEXT),
-	ENTITY_AMOUNT("# Entities", EntityAmountFilter.class, Format.NUMBER);
+	ENTITY_AMOUNT("#Entities", EntityAmountFilter.class, Format.NUMBER);
 
 	private String string;
 	private Class<? extends Filter> clazz;
