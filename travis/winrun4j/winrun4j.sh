c319748949bacd0740489a9d8c31f39cb0600ebd
Querz <admin@querz.net>
2021-04-12 14:44:47 +0200

    change paths

--- travis/winrun4j/winrun4j.sh
+++ travis/winrun4j/winrun4j.sh
@@ -13,12 +13,13 @@ wget https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe 
 mkdir -p ~/".wine/drive_c/winrun4j"
 cp winrun4j/bin/WinRun4J64.exe ~/".wine/drive_c/winrun4j"
 cp winrun4j/bin/RCEDIT64.exe ~/".wine/drive_c/winrun4j"
+cp ~/"inno/icon.ico" ~/".wine/drive_c/winrun4j"
+cp ~/"travis/winrun4j/winrun4j.ini" ~/".wine/drive_c/winrun4j"
 cp rcedit.exe ~/".wine/drive_c/winrun4j"
 
-# add config to WinRun4J.exe
 echo "assembling winrun4j"
-wine "C:\winrun4j\RCEDIT64.exe" "/I" "WinRun4J64.exe" "$(winepath -w "~/inno/icon.ico")"
-wine "C:\winrun4j\RCEDIT64.exe" "/N" "WinRun4J64.exe" "$(winepath -w "~/travis/winrun4j/winrun4j.ini")"
+wine "C:\winrun4j\RCEDIT64.exe" "/I" "WinRun4J64.exe" "icon.ico"
+wine "C:\winrun4j\RCEDIT64.exe" "/N" "WinRun4J64.exe" "winrun4j.ini"
 wine "C:\winrun4j\rcedit.exe" "WinRun4J64.exe" "--set-file-version" "1.0.0.0" "--set-product-version" "1.15.3" "--set-version-string" "LegalCopyright" "Querz"
 mkdir -p ~/"build/winrun4j"
 cp ~/".wine/drive_c/winrun4j/WinRun4J.exe" ~/"build/winrun4j/MCA Selector.exe"
