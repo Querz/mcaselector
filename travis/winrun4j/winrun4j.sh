9fac970e5f37438c5aac1959b6ac28a413b965fb
Querz <admin@querz.net>
2021-04-12 14:36:19 +0200

    use winrun4j to launch jar on windows

---
+++ travis/winrun4j/winrun4j.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -fr build/tmp/winrun4j
+mkdir -p build/tmp/winrun4j
+cd build/tmp/winrun4j
+
+wget https://github.com/poidasmith/winrun4j/files/1822558/winrun4J-0.4.5.zip -O winrun4j.zip
+unzip winrun4j.zip
+rm winrun4j.zip
+
+wget https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe -O rcedit.exe
+
+mkdir -p ~/".wine/drive_c/winrun4j"
+cp winrun4j/bin/WinRun4J64.exe ~/".wine/drive_c/winrun4j"
+cp winrun4j/bin/RCEDIT64.exe ~/".wine/drive_c/winrun4j"
+cp rcedit.exe ~/".wine/drive_c/winrun4j"
+
+# add config to WinRun4J.exe
+echo "assembling winrun4j"
+wine "C:\winrun4j\RCEDIT64.exe" "/I" "WinRun4J64.exe" "$(winepath -w "~/inno/icon.ico")"
+wine "C:\winrun4j\RCEDIT64.exe" "/N" "WinRun4J64.exe" "$(winepath -w "~/travis/winrun4j/winrun4j.ini")"
+wine "C:\winrun4j\rcedit.exe" "WinRun4J64.exe" "--set-file-version" "1.0.0.0" "--set-product-version" "1.15.3" "--set-version-string" "LegalCopyright" "Querz"
+mkdir -p ~/"build/winrun4j"
+cp ~/".wine/drive_c/winrun4j/WinRun4J.exe" ~/"build/winrun4j/MCA Selector.exe"
