5591008fc82e300f61c82c185ce28c6da5544f6b
Querz <admin@querz.net>
2021-04-13 16:27:11 +0200

    use resourcehacker installer

--- travis/winrun4j/winrun4j.sh
+++ travis/winrun4j/winrun4j.sh
@@ -7,9 +7,7 @@ wget https://github.com/poidasmith/winrun4j/files/1822558/winrun4J-0.4.5.zip -O 
 unzip build/tmp/winrun4j/winrun4j.zip -d build/tmp/winrun4j/
 rm build/tmp/winrun4j/winrun4j.zip
 
-wget http://www.angusj.com/resourcehacker/resource_hacker.zip -O build/tmp/winrun4j/resourcehacker.zip
-unzip build/tmp/winrun4j/resourcehacker.zip -d build/tmp/winrun4j/
-rm build/tmp/winrun4j/resourcehacker.zip
+wget http://www.angusj.com/resourcehacker/reshacker_setup.exe -O build/tmp/winrun4j/resourcehackersetup.exe
 
 mkdir -p ~/".wine/drive_c/winrun4j"
 cp build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe ~/".wine/drive_c/winrun4j"
@@ -30,11 +28,12 @@ WINEARCH=win32
 wine config
 
 mkdir -p ~/".wine/drive_c/winrun4j"
-cp build/tmp/winrun4j/ResourceHacker.exe ~/".wine/drive_c/winrun4j"
+cp build/tmp/winrun4j/resourcehackersetup.exe ~/".wine/drive_c/winrun4j"
 cp travis/winrun4j/resource.rc ~/".wine/drive_c/winrun4j"
 
-wine "C:\winrun4j\ResourceHacker.exe" "-open C:\winrun4j\resource.rc -save C:\winrun4j\resource.rec -action compile"
-wine "C:\winrun4j\ResourceHacker.exe" "-open $exePath -save $exePath -action add -resource C:\winrun4j\resource.rec"
+wine "C:\winrun4j\resourcehackersetup.exe"
+wine "ResourceHacker" "-open C:\winrun4j\resource.rc -save C:\winrun4j\resource.rec -action compile"
+wine "ResourceHacker" "-open $exePath -save $exePath -action add -resource C:\winrun4j\resource.rec"
 
 # reset to 64 bit
 export WINEARCH=win64
