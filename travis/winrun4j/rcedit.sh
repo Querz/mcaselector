4b76ea87cea603f1e7db94fd29e55645d45dec56
Querz <admin@querz.net>
2021-04-14 12:08:44 +0200

    remove vars

--- travis/winrun4j/rcedit.sh
+++ travis/winrun4j/rcedit.sh
@@ -1,19 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-ls -lah build/inno
-echo "hello"
-exePath=$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")
-echo "$exePath"
-echo "made WinRun4J64.exe path"
-iconPath=$(winepath -w "$1")
-echo "$iconPath"
-echo "made icon path"
-iniPath=$(winepath -w "$2")
-echo "$iniPath"
-echo "made ini path"
 echo "clearing exe"
-wine "C:\winrun4j\RCEDIT64.exe" "/C" "$exePath"
+wine "C:\winrun4j\RCEDIT64.exe" "/C" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")"
 echo "adding icon"
-wine "C:\winrun4j\RCEDIT64.exe" "/I" "$exePath" "$iconPath"
+wine "C:\winrun4j\RCEDIT64.exe" "/I" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "$(winepath -w "inno/icon.ico")"
 echo "adding ini"
-wine "C:\winrun4j\RCEDIT64.exe" "/N" "$exePath" "$iniPath"
+wine "C:\winrun4j\RCEDIT64.exe" "/N" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "$(winepath -w "travis/winrun4j/winrun4j.ini")"
--- travis/winrun4j/resourcehacker.sh
+++ travis/winrun4j/resourcehacker.sh
@@ -1,11 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "hacking resources"
-exePath=$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")
 echo "compiling resource.rc"
-wine "C:\winrun4j\ResourceHacker.exe" "-open C:\winrun4j\resource.rc -save C:\winrun4j\resource.res -action compile"
+wine "C:\winrun4j\ResourceHacker.exe" "-open" "C:\winrun4j\resource.rc" "-save" "C:\winrun4j\resource.res" "-action" "compile"
 echo "applying resource.res"
-wine "C:\winrun4j\ResourceHacker.exe" "-open $exePath -save $exePath -action add -resource C:\winrun4j\resource.res"
+wine "C:\winrun4j\ResourceHacker.exe" "-open" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "-save" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "-action" "add" "-resource C:\winrun4j\resource.res"
 
 echo "copying finished MCA Selector.exe"
 mkdir -p build/winrun4j
