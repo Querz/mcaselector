bf16405899412adc16b086783c1afb7db056b822
Querz <admin@querz.net>
2021-04-14 11:42:42 +0200

    even more debugs

--- travis/winrun4j/rcedit.sh
+++ travis/winrun4j/rcedit.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 ls -lah build/inno
+echo "hello"
 exePath=$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")
+echo "$exePath"
+echo "made WinRun4J64.exe path"
 iconPath=$(winepath -w "$1")
+echo "$iconPath"
+echo "made icon path"
 iniPath=$(winepath -w "$2")
+echo "$iniPath"
+echo "made ini path"
 echo "clearing exe"
 wine "C:\winrun4j\RCEDIT64.exe" "/C" "$exePath"
 echo "adding icon"
