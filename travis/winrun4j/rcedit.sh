61b1e2dfc1c89428e684123017e902d33067242c
Querz <admin@querz.net>
2021-04-14 12:17:11 +0200

    testing git status

--- travis/winrun4j/rcedit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "clearing exe"
-wine "C:\winrun4j\RCEDIT64.exe" "/C" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")"
-echo "adding icon"
-wine "C:\winrun4j\RCEDIT64.exe" "/I" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "$(winepath -w "inno/icon.ico")"
-echo "adding ini"
-wine "C:\winrun4j\RCEDIT64.exe" "/N" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "$(winepath -w "travis/winrun4j/winrun4j.ini")"
--- travis/winrun4j/winrun4jinstall.sh
+++ travis/winrun4j/winrun4jinstall.sh
@@ -12,9 +12,25 @@ unzip build/tmp/winrun4j/resourcehacker.zip -d build/tmp/winrun4j/
 rm build/tmp/winrun4j/resourcehacker.zip
 
 mkdir -p ~/".wine/drive_c/winrun4j"
-cp build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe ~/".wine/drive_c/winrun4j"
 cp build/tmp/winrun4j/winrun4j/bin/RCEDIT64.exe ~/".wine/drive_c/winrun4j"
 cp inno/icon.ico ~/".wine/drive_c/winrun4j"
 cp travis/winrun4j/winrun4j.ini ~/".wine/drive_c/winrun4j"
 cp build/tmp/winrun4j/ResourceHacker.exe ~/".wine/drive_c/winrun4j"
 cp travis/winrun4j/resource.rc ~/".wine/drive_c/winrun4j"
+
+echo "clearing exe"
+wine "C:\winrun4j\RCEDIT64.exe" "/C" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")"
+echo "adding icon"
+wine "C:\winrun4j\RCEDIT64.exe" "/I" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "C:\winrun4j\icon.ico"
+echo "adding ini"
+wine "C:\winrun4j\RCEDIT64.exe" "/N" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "C:\winrun4j\winrun4j.ini"
+
+
+echo "compiling resource.rc"
+wine "C:\winrun4j\ResourceHacker.exe" "-open" "C:\winrun4j\resource.rc" "-save" "C:\winrun4j\resource.res" "-action" "compile"
+echo "applying resource.res"
+wine "C:\winrun4j\ResourceHacker.exe" "-open" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "-save" "$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")" "-action" "add" "-resource C:\winrun4j\resource.res"
+
+echo "copying finished MCA Selector.exe"
+mkdir -p build/winrun4j
+cp build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe "build/inno/MCA Selector.exe"
