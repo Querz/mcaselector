ff1355158383084039bfd5d6300847a928e49caf
Querz <admin@querz.net>
2021-04-14 11:27:39 +0200

    add some debugs

--- travis/winrun4j/rcedit.sh
+++ travis/winrun4j/rcedit.sh
@@ -3,6 +3,9 @@
 exePath=$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")
 iconPath=$(winepath -w "$1")
 iniPath=$(winepath -w "$2")
+echo "clearing exe"
 wine "C:\winrun4j\RCEDIT64.exe" "/C" "$exePath"
+echo "adding icon"
 wine "C:\winrun4j\RCEDIT64.exe" "/I" "$exePath" "$iconPath"
+echo "adding ini"
 wine "C:\winrun4j\RCEDIT64.exe" "/N" "$exePath" "$iniPath"
--- travis/winrun4j/resourcehacker.sh
+++ travis/winrun4j/resourcehacker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "hacking resources"
 exePath=$(winepath -w "build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe")
-wine "C:\winrun4j\ResourceHacker.exe"
+echo "compiling resource.rc"
 wine "C:\winrun4j\ResourceHacker.exe" "-open C:\winrun4j\resource.rc -save C:\winrun4j\resource.res -action compile"
+echo "applying resource.res"
 wine "C:\winrun4j\ResourceHacker.exe" "-open $exePath -save $exePath -action add -resource C:\winrun4j\resource.res"
 
+echo "copying finished MCA Selector.exe"
 mkdir -p build/winrun4j
 cp build/tmp/winrun4j/winrun4j/bin/WinRun4J64.exe "build/inno/MCA Selector.exe"
