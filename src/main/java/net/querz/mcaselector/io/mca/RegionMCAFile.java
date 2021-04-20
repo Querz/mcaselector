b4510cb98aee82d3cb1947f1c82fa12551124708
Querz <admin@querz.net>
2021-04-20 10:20:31 +0200

    remove unused function

--- src/main/java/net/querz/mcaselector/io/mca/RegionMCAFile.java
+++ src/main/java/net/querz/mcaselector/io/mca/RegionMCAFile.java
@@ -1,9 +1,7 @@
 package net.querz.mcaselector.io.mca;
 
-import net.querz.mcaselector.debug.Debug;
 import net.querz.mcaselector.point.Point2i;
 import net.querz.mcaselector.range.Range;
-import net.querz.mcaselector.tiles.overlay.OverlayParser;
 import net.querz.nbt.tag.CompoundTag;
 import java.io.File;
 import java.util.List;
@@ -33,21 +31,5 @@ public class RegionMCAFile extends MCAFile<RegionChunk> {
 	@Override
 	public void mergeChunksInto(MCAFile<RegionChunk> destination, Point2i offset, boolean overwrite, Set<Point2i> sourceChunks, Set<Point2i> selection, List<Range> ranges) {
 		mergeChunksInto(destination, offset, overwrite, sourceChunks, selection, ranges, RegionMCAFile::newEmptyChunk);
-	}
-
-	public long[] parseData(OverlayParser parser) {
-		long[] data = new long[1024];
-		for (int i = 0; i < 1024; i++) {
-			RegionChunk chunk = getChunk(i);
-			if (chunk != null) {
-				try {
-					data[i] = parser.parseValue(new ChunkData(chunk, null, null));
-				} catch (Exception ex) {
-					Point2i chunkLocation = getLocation().regionToChunk().add(i % 32, (i - (i % 32)) / 32);
-					Debug.dumpException(String.format("failed to parse %s overlay value in chunk %s", parser.name(), chunkLocation), ex);
-				}
-			}
-		}
-		return data;
 	}
 }
