383c00d212ace11e45b6d2abfa7381d17cb94d3d
Querz <admin@querz.net>
2021-04-23 13:07:51 +0200

    don't use sdkman to install jdk for travis

---
+++ travis/jdkinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://cdn.azul.com/zulu/bin/zulu8.52.0.23-ca-fx-jdk8.0.282-linux_x64.tar.gz -O ~/jdkfx8.tar.gz
+tar -xf ~/jdkfx8.tar.gz -C ~/
+rm ~/jdkfx8.tar.gz
+export JAVA_HOME="~/zulu8.52.0.23-ca-fx-jdk8.0.282-linux_x64"
