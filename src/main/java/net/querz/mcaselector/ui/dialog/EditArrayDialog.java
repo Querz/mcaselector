3475283f6ed78943ffcbe4bedabdf050729d274c
Querz <info@querz.net>
2021-10-30 12:35:18 +0200

    add support for 1-3 bits in array editor

--- src/main/java/net/querz/mcaselector/ui/dialog/EditArrayDialog.java
+++ src/main/java/net/querz/mcaselector/ui/dialog/EditArrayDialog.java
@@ -477,6 +477,9 @@ public class EditArrayDialog<T> extends Dialog<EditArrayDialog.Result> {
 
 	private enum BitCount {
 		NONE(-1, ""),
+		ONE(1),
+		TWO(2),
+		THREE(3),
 		FOUR(4),
 		FIVE(5),
 		SIX(6),
