5315e80f8db8f330f51300ea8ecf1c099d88a02a
Querz <info@querz.net>
2021-11-01 11:18:09 +0100

    use version specific implementation for tile entities for overlay

--- src/main/java/net/querz/mcaselector/tiles/overlay/TileEntityAmountParser.java
+++ src/main/java/net/querz/mcaselector/tiles/overlay/TileEntityAmountParser.java
@@ -1,6 +1,9 @@
 package net.querz.mcaselector.tiles.overlay;
 
 import net.querz.mcaselector.io.mca.ChunkData;
+import net.querz.mcaselector.version.ChunkFilter;
+import net.querz.mcaselector.version.VersionController;
+import net.querz.nbt.tag.CompoundTag;
 import net.querz.nbt.tag.ListTag;
 import net.querz.nbt.tag.LongArrayTag;
 import net.querz.nbt.tag.Tag;
@@ -13,14 +16,15 @@ public class TileEntityAmountParser extends AmountParser {
 
 	@Override
 	public int parseValue(ChunkData chunkData) {
-		if (chunkData.getRegion() == null || chunkData.getRegion().getData() == null) {
+		if (chunkData.getRegion() == null) {
 			return 0;
 		}
-		Tag<?> rawTileEntities = chunkData.getRegion().getData().getCompoundTag("Level").get("TileEntities");
-		if (rawTileEntities == null || rawTileEntities.getID() == LongArrayTag.ID) {
+		ChunkFilter chunkFilter = VersionController.getChunkFilter(chunkData.getRegion().getData().getInt("DataVersion"));
+		ListTag<CompoundTag> tileEntities = chunkFilter.getTileEntities(chunkData.getRegion().getData());
+		if (tileEntities == null) {
 			return 0;
 		}
-		return ((ListTag<?>) rawTileEntities).asCompoundTagList().size();
+		return tileEntities.size();
 	}
 
 	@Override
