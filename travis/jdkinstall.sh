1e2ad92e9a00e953c557c697a2f2630c749d1724
Querz <info@querz.net>
2021-12-15 19:58:44 +0100

    upgrade to gradle 7.3.2 and java 17

--- travis/jdkinstall.sh
+++ travis/jdkinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://cdn.azul.com/zulu/bin/zulu16.30.15-ca-fx-jdk16.0.1-linux_x64.tar.gz -O ~/jdkfx16.tar.gz
-tar -xf ~/jdkfx16.tar.gz -C ~/
-rm ~/jdkfx16.tar.gz
-mv ~/zulu16.30.15-ca-fx-jdk16.0.1-linux_x64 ~/jdkfx16
+wget https://cdn.azul.com/zulu/bin/zulu17.30.15-ca-fx-jdk17.0.1-linux_x64.tar.gz -O ~/jdkfx17.tar.gz
+tar -xf ~/jdkfx17.tar.gz -C ~/
+rm ~/jdkfx17.tar.gz
+mv ~/zulu17.30.15-ca-fx-jdk17.0.1-linux_x64 ~/jdkfx17
