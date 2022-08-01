3738e76bf8eb097509de321d168c710c67612301
HoldYourWaffle <ravivanrooijen@live.nl>
2022-08-01 16:25:43 +0200

    Fail fast in ChunkData.getDataVersion

--- src/main/java/net/querz/mcaselector/io/mca/ChunkData.java
+++ src/main/java/net/querz/mcaselector/io/mca/ChunkData.java
@@ -4,6 +4,7 @@ import net.querz.mcaselector.changer.Field;
 import net.querz.mcaselector.point.Point3i;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public record ChunkData(RegionChunk region, PoiChunk poi, EntitiesChunk entities) {
 
@@ -13,6 +14,7 @@ public record ChunkData(RegionChunk region, PoiChunk poi, EntitiesChunk entities
 	}
 
 	private boolean relocateChunk(Chunk c, Point3i offset) {
+		// MAINTAINER why do we check if DataVersion exists, shouldn't that be a (fast-failing) irrecoverable corruption?
 		if (c != null && c.getData() != null && c.getData().containsKey("DataVersion")) {
 			return c.relocate(offset);
 		}
@@ -34,11 +36,15 @@ public record ChunkData(RegionChunk region, PoiChunk poi, EntitiesChunk entities
 			source = entities;
 		} else {
 			// MAINTAINER why not check poi data?
-			// MAINTAINER fail fast?
-			return 0;
+			throw new IllegalStateException("ChunkData does not contain any NBT data");
 		}
 
-		return source.getData().getInt("DataVersion");
+		int dataVersion = source.getData().getInt("DataVersion");
+		if (dataVersion == 0) {
+			// FIXME upstream NBT's primitive getters should fail fast instead
+			throw new NoSuchElementException("Chunk NBT does not have DataVersion property");
+		}
+		return dataVersion;
 	}
 
 }
