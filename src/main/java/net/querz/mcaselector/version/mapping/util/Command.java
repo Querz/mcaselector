121109e0aa8c695a353cf1082038365c14e5b7d9
Querz <admin@querz.net>
2025-01-15 18:09:19 +0100

    add functionality to run mc server

--- src/main/java/net/querz/mcaselector/version/mapping/util/Command.java
+++ src/main/java/net/querz/mcaselector/version/mapping/util/Command.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public final class Command {
 
@@ -14,6 +18,39 @@ public final class Command {
 		System.out.println(pb.command());
 		pb.directory(workingDirectory.toFile());
 		Process pr = pb.start();
+		BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+		String line;
+		while ((line = buf.readLine()) != null) {
+			System.out.println(line);
+		}
+		BufferedReader errBuf = new BufferedReader(new InputStreamReader(pr.getErrorStream()));
+		while ((line = errBuf.readLine()) != null) {
+			System.out.println(line);
+		}
+		pr.waitFor();
+	}
+
+	public static void redirect(Path workingDirectory, String... cmd) throws InterruptedException, IOException {
+		String[] a = null, b = null;
+		for (int i = 0; i < cmd.length; i++) {
+			if (cmd[i].equals("|")) {
+				a = new String[i];
+				System.arraycopy(cmd, 0, a, 0, i);
+				b = new String[cmd.length - i - 1];
+				System.arraycopy(cmd, i + 1, b, 0, b.length);
+				break;
+			}
+		}
+		if (a == null) {
+			throw new IllegalArgumentException("expected pipe");
+		}
+
+		List<Process> processes = ProcessBuilder.startPipeline(List.of(
+				new ProcessBuilder(a).directory(workingDirectory.toFile()).inheritIO().redirectOutput(ProcessBuilder.Redirect.PIPE),
+				new ProcessBuilder(b).directory(workingDirectory.toFile()).redirectError(ProcessBuilder.Redirect.INHERIT)
+		));
+
+		Process pr = processes.getLast();
 		BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 		String line;
 		while ((line = buf.readLine()) != null) {
