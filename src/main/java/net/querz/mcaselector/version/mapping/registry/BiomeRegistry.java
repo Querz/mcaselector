8b474830cd9f341e6a97df5ee7306edaf52dc4c1
Querz <info@querz.net>
2025-02-03 02:43:26 +0100

    cleanup BiomeRegistry

--- src/main/java/net/querz/mcaselector/version/mapping/registry/BiomeRegistry.java
+++ src/main/java/net/querz/mcaselector/version/mapping/registry/BiomeRegistry.java
@@ -3,54 +3,35 @@ package net.querz.mcaselector.version.mapping.registry;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import net.querz.mcaselector.filter.filters.BiomeFilter;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.querz.mcaselector.io.FileHelper;
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
 public final class BiomeRegistry {
 
-	private static final Logger LOGGER = LogManager.getLogger(BiomeRegistry.class);
-
 	private BiomeRegistry() {}
-
-	private static final Map<Integer, String> idMapping = new HashMap<>();
-	private static final Map<String, Integer> nameMapping = new HashMap<>();
-	private static final Set<String> mapping = new HashSet<>();
 
 	private static final Gson GSON = new GsonBuilder()
 			.setPrettyPrinting()
 			.create();
 
+	private static final Map<String, Integer> nameMapping = FileHelper.loadFromResource("mapping/registry/biome_name_to_id.json", p -> {
+		try (BufferedReader reader = Files.newBufferedReader(p)) {
+			return GSON.fromJson(reader, new TypeToken<Map<String, Integer>>(){}.getType());
+		}
+	});
+	private static final Map<Integer, String> idMapping = new HashMap<>();
 	static {
-		try (BufferedReader bis = new BufferedReader(new InputStreamReader(
-				Objects.requireNonNull(BiomeFilter.class.getClassLoader().getResourceAsStream("mapping/registry/biome_name_to_id.json"))))) {
-			Map<String, Integer> biomeToID = GSON.fromJson(bis, new TypeToken<Map<String, Integer>>(){}.getType());
-			for (Map.Entry<String, Integer> entry : biomeToID.entrySet()) {
-				idMapping.put(entry.getValue(), "minecraft:" + entry.getKey());
-				nameMapping.put("minecraft:" + entry.getKey(), entry.getValue());
-			}
-		} catch (IOException ex) {
-			LOGGER.error("error reading mapping/registry/biome_name_to_id.json", ex);
-		}
-		try (BufferedReader bis = new BufferedReader(new InputStreamReader(
-				Objects.requireNonNull(BiomeFilter.class.getClassLoader().getResourceAsStream("mapping/registry/biome.json"))))) {
-			List<String> status = GSON.fromJson(bis, new TypeToken<List<String>>(){}.getType());
-			for (String s : status) {
-				mapping.add("minecraft:" + s);
-			}
-		} catch (IOException ex) {
-			LOGGER.error("error reading mapping/all_biome_names.txt", ex);
-		}
+		nameMapping.forEach((k, v) -> idMapping.put(v, k));
 	}
+	private static final Set<String> mapping = FileHelper.loadFromResource("mapping/registry/biome.json", p -> {
+		try (BufferedReader reader = Files.newBufferedReader(p)) {
+			return GSON.fromJson(reader, new TypeToken<Set<String>>(){}.getType());
+		}
+	});
 
 	public static boolean isValidName(String name) {
 		return mapping.contains(name);
