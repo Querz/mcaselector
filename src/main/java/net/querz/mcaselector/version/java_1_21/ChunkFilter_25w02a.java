afbf523504141f1ac360312c83888ad30120cbea
Querz <info@querz.net>
2025-03-19 01:55:00 +0100

    change version when to apply new force blending 'hack' to 25w09a

--- src/main/java/net/querz/mcaselector/version/java_1_21/ChunkFilter_25w02a.java
+++ src/main/java/net/querz/mcaselector/version/java_1_21/ChunkFilter_25w02a.java
@@ -8,12 +8,12 @@ import net.querz.nbt.CompoundTag;
 
 public class ChunkFilter_25w02a {
 
-	@MCVersionImplementation(4298)
+	@MCVersionImplementation(4317)
 	public static class Blending implements ChunkFilter.Blending {
 
 		@Override
 		public void forceBlending(ChunkData data) {
-			// force blending chunks of the same version no longer works in 25w02a, but we can reset the DataVersion
+			// force blending chunks of the same version no longer works in 25w09a, but we can reset the DataVersion
 			// to 1.21.4. New features such as leaf litter will stay in the chunk.
 			CompoundTag root = Helper.getRegion(data);
 			Helper.setDataVersion(root, 4189);
